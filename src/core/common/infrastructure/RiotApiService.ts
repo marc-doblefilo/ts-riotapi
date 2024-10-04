2e24885e627c0d9e4fc84147dc86e88b0e1caa48
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2024-10-04 18:06:25 +0200

    refactor: generate url in riot api service

--- src/core/common/infrastructure/RiotApiService.ts
+++ src/core/common/infrastructure/RiotApiService.ts
@@ -1,14 +1,33 @@
+import { EndpointsEnum } from '../../../utils/EndpointEnum';
 import { AccountV1 } from '../../account-v1/domain/AccountV1';
 import { IRiotApiService } from '../domain/IRiotApiService';
 import { Regions } from '../domain/Regions';
 
 export class RiotApiService implements IRiotApiService {
-  private readonly baseUrl = 'api.riotgames.com/riot/account';
+  private readonly baseUrl = 'api.riotgames.com';
 
   constructor(private readonly apiKey: string) {}
 
   private getBaseUrlByRegion(region: Regions): string {
     return `https://${region}.${this.baseUrl}`;
+  }
+
+  private generateUrl(query: {
+    baseUrl: string;
+    endpoint: EndpointsEnum;
+    params: Record<string, string>;
+  }): URL {
+    const path = query.endpoint.replace(/\$\{(\w+)\}/g, (_, key) => {
+      if (query.params[key] === undefined) {
+        throw new Error(`Missing parameter: ${key}`);
+      }
+      return query.params[key];
+    });
+
+    // Construct the full URL using the base URL and the dynamically generated path
+    const url = new URL(`/riot/account${path}`, query.baseUrl);
+    url.searchParams.append('api_key', this.apiKey);
+    return url;
   }
 
   public async getAccountByRiotId(query: {
@@ -18,7 +37,13 @@ export class RiotApiService implements IRiotApiService {
   }): Promise<AccountV1> {
     const { region, gameName, tagLine } = query;
 
-    const url = `${this.getBaseUrlByRegion(region)}/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${this.apiKey}`;
+    const url = this.generateUrl({
+      baseUrl: this.getBaseUrlByRegion(region),
+      endpoint: EndpointsEnum.ACCOUNTV1BYID,
+      params: { gameName, tagLine },
+    });
+
+    console.info(url);
 
     const response = await fetch(url);
 
---
+++ src/utils/EndpointEnum.ts
@@ -0,0 +1,3 @@
+export enum EndpointsEnum {
+  ACCOUNTV1BYID = '/v1/accounts/by-riot-id/${gameName}/${tagLine}',
+}
